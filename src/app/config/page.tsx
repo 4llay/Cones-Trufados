44f28bea3d1e775741ad44edb37493e067186fbe
4llay <4llayjpcg@gmail.com>
2023-09-12 15:56:05 -0300

    config v2

--- src/app/config/page.tsx
+++ src/app/config/page.tsx
@@ -7,18 +7,18 @@ export default function Config() {
     return (
 
         <main className="w-full h-full flex justify-center pt-[50px]">
-            <div className="w-[50dvw] bg-slate-100 dark:bg-DarkMenuColor p-[30px] rounded-md flex flex-col items-center">
+            <div className="w-[90dvw] lg:w-[50dvw] bg-gradient-to-t from-cianinho to-rosinha border-4 dark:from-DarkMenuColor dark:to-DarkMenuColor p-[30px] rounded-md flex flex-col items-center">
                     <h1 className="text-[40px]">Tema</h1>
-                <div className="flex justify-center gap-[50px] text-[30px]">
-                    <button onClick={() => setTheme("light")} className="w-[93px]">
+                <div className="flex flex-col justify-center gap-[20px] sm:flex-row sm:gap-[50px] sm:text-[30px] bg-slate-200 dark:bg-slate-800 p-2 sm:pr-8 rounded-md">
+                    <button onClick={() => setTheme("light")} className="w-[105px] border-4 border-cianinho dark:border-transparent rounded-sm">
                         <i className="fa-solid fa-sun"></i>
                         <h1>Claro</h1>
                     </button>
-                    <button onClick={() => setTheme("dark")} className="w-[93px]">
+                    <button onClick={() => setTheme("dark")} className="w-[105px] border-4 border-transparent dark:border-red-800  rounded-sm">
                         <i className="fa-solid fa-moon"></i>
                         <h1>Escuro</h1>
                     </button>
-                    <button onClick={() => setTheme("system")} className="w-[93px]">
+                    <button onClick={() => setTheme("system")} className="w-[105px]">
                         <i className="fa-solid fa-desktop"></i>
                         <h1>Sistema</h1>
                     </button>
